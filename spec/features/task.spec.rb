dbff229022b261a497a5a12e202a9c4cc8930a8b
jeanarm <nizigiarmel2015@gmail.com>
2019-09-16 09:01:00 +0200

    from 14 to step 23

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 # On the right side of this RSpec.feature, write the test item name like "task management feature" (grouped by do ~ end)
 RSpec.feature "Task management function", type: :feature do
-  # In scenario (alias of it), write the processing of the test for each item you want to check.
-  scenario "Test task list" do
-      # Create two tasks in advance to use in the task list test
-      FactoryBot.create(:task, title: 'Added name 1')
-      FactoryBot.create(:task, title: 'Added name 2')
-      FactoryBot.create(:second_task, title: 'Added name 3', content: 'Added content')
-    end
+  # In scenario (alias of it), write the processing of the test for each item you want to check
+  background  do
+    User.create!( email: 'na@gmail.Com',  password: '1234567', name: "Armel" )
+end
+scenario "Test task list" do
+    visit  root_path
+    fill_in  'Email' ,  with: 'na@gmail.Com'
+    fill_in  'Password' ,  with: '1234567'
+    click_on  'Log in'
+    expect(page ).to have_text('Signed in successfully.')
+    click_on 'New Task'
+    fill_in  'Tittle' ,  with: 'grettings'
+    fill_in  'Content' ,  with: 'testtest'
+    click_on 'Create Task'
+    expect(page).to have_content 'testtest'
+end
 
   # visit to tasks_path (transition to task list page)
   visit tasks_path
 
   # write a test to verify that the string "" testtesttest "" samplesample "is included when accessing the task list page using have_content method
 
-  expect(page).to have_content 'testtesttest'
-  expect(page).to have_content 'samplesample'
+
 
   end
 
