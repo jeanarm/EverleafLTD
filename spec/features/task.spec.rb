64f2410a1586de0fb8ae781a879acffc03692c2f
jeanarm <nizigiarmel2015@gmail.com>
2019-09-17 15:18:55 +0200

    heroku errors fixed and rspec test

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,35 +4,55 @@ require 'rails_helper'
 RSpec.feature "Task management function", type: :feature do
   # In scenario (alias of it), write the processing of the test for each item you want to check
   background  do
-    User.create!( email: 'na@gmail.Com',  password: '1234567', name: "Armel" )
+    User.create!(name: "Armel", email: 'ni@gmail.Com',  password: '1234567')
+    User.create!(name: "Nina", email: 'na@gmail.Com',  password: '1234567')
+    
 end
 scenario "Test task list" do
     visit  root_path
-    fill_in  'Email' ,  with: 'na@gmail.Com'
+    fill_in  'Email',  with: 'ni@gmail.Com'
     fill_in  'Password' ,  with: '1234567'
     click_on  'Log in'
-    expect(page ).to have_text('Signed in successfully.')
-    click_on 'New Task'
-    fill_in  'Tittle' ,  with: 'grettings'
+    expect(page).to have_text('Signed in successfully.')
+    visit  new_task_path
+    fill_in  'Title' ,  with: 'grettings'
     fill_in  'Content' ,  with: 'testtest'
     click_on 'Create Task'
     expect(page).to have_content 'testtest'
 end
 
   # visit to tasks_path (transition to task list page)
-  visit tasks_path
+ # visit tasks_path
 
   # write a test to verify that the string "" testtesttest "" samplesample "is included when accessing the task list page using have_content method
 
-
-
-  end
-
   scenario "Test task creation" do
-
+    visit  root_path 
+    fill_in  'Email',  with: 'na@gmail.Com'
+    fill_in  'Password',  with: '1234567'
+    click_on  'Log in'
+    expect(page ).to have_text('Signed in successfully.') 
+   visit  new_task_path
+    fill_in  'Title' ,  with: 'grettings' 
+    fill_in  'Content' ,  with: 'testtesttest'
+    click_on 'Create Task'
+    expect(page).to have_text('Task was successfully created.')
   end
-
   scenario "Test task details" do
+    visit  root_path 
+    fill_in  'Email',  with: 'na@gmail.Com'
+    fill_in  'Password',  with: '1234567'
+    click_on  'Log in'
+    expect(page ).to have_text('Signed in successfully.') 
+   visit  new_task_path
+    fill_in  'Title' ,  with: 'grettings' 
+    fill_in  'Content' ,  with: 'test'
+    click_on 'Create Task'
+    click_on 'Back'
+    click_on 'Show'
+    expect(page).to have_text('test')
 
   end
+ 
 end
+
