e4cb741c56729031045a0ef4805846248aa7d141
jeanarm <nizigiarmel2015@gmail.com>
2019-10-23 12:04:10 +0200

    fixing issues

--- app/models/usr.rb
+++ app/models/usr.rb
@@ -2,7 +2,7 @@ class Usr < ApplicationRecord
     has_many :tasks, dependent: :destroy
     has_many :labels, dependent: :destroy
     validates :name, presence: true, length: {maximum: 40}
-   validates :email, presence: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+   validates :email, uniqueness: true, presence: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
    before_validation { email.downcase! }
    has_secure_password
   validates :password, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2019_10_22_115031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "labelings", force: :cascade do |t|
-    t.bigint "label_id"
-    t.bigint "task_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["label_id"], name: "index_labelings_on_label_id"
-    t.index ["task_id"], name: "index_labelings_on_task_id"
-  end
-
   create_table "labels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -64,8 +55,6 @@ ActiveRecord::Schema.define(version: 2019_10_22_115031) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "labelings", "labels"
-  add_foreign_key "labelings", "tasks"
   add_foreign_key "labels", "usrs"
   add_foreign_key "tasks", "usrs"
   add_foreign_key "tasks_labels", "labels"
